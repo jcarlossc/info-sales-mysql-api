b65e9eb6f2d7bcfdd223ce085fe130497d220fac
jcarlossc <jcarlossc1977@gmail.com>
2026-07-06 14:58:25 -0300

    feat: Validações de load_all_config adicionadas

--- src/info_sales_mysql_api/utils/load_yaml/loader_yaml.py
+++ src/info_sales_mysql_api/utils/load_yaml/loader_yaml.py
@@ -6,10 +6,18 @@ def load_all_configs(config_path: Path) -> Dict[str, Any]:
 
     configs: Dict[str, Any] = {}
 
+    if not config_path.exists():
+        raise FileNotFoundError(
+            f"Diretório de configuração não encontrado: {config_path}"
+        )
+
+    if not config_path.is_dir():
+        raise NotADirectoryError(
+            f"Caminho informado não é diretório: {config_path}"
+        )
+
     for file in config_path.glob("*.yaml"):
-
         with open(file, "r", encoding="utf-8") as f:
-
             configs[file.stem] = yaml.safe_load(f)
 
     return configs
