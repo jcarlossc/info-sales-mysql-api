9c7a287a94120f03c1eeda69f76dd786ff1bff9d
jcarlossc <jcarlossc1977@gmail.com>
2026-07-11 12:55:56 -0300

    test: Teste adicionado

---
+++ tests/test_settings.py
@@ -0,0 +1,21 @@
+from info_sales_mysql_api.utils.config_env.Settings import Settings
+
+
+def test_settings_load(monkeypatch):
+    """
+    Deve carregar corretamente as variáveis de ambiente.
+    """
+
+    monkeypatch.setenv("MYSQL_HOST", "localhost")
+    monkeypatch.setenv("MYSQL_PORT", "3306")
+    monkeypatch.setenv("MYSQL_DATABASE", "empresa")
+    monkeypatch.setenv("MYSQL_USER", "root")
+    monkeypatch.setenv("MYSQL_PASSWORD", "123456")
+
+    settings = Settings()
+
+    assert settings.mysql_host == "localhost"
+    assert settings.mysql_port == 3306
+    assert settings.mysql_database == "empresa"
+    assert settings.mysql_user == "root"
+    assert settings.mysql_password == "123456"
