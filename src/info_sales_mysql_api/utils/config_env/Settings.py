784cf272f485c03b3cbbff8758862a003822795c
jcarlossc <jcarlossc1977@gmail.com>
2026-07-11 12:40:15 -0300

    docs: Comentários adicionados

--- src/info_sales_mysql_api/utils/config_env/Settings.py
+++ src/info_sales_mysql_api/utils/config_env/Settings.py
@@ -2,16 +2,22 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    # Endereço do servidor MySQL.
     mysql_host: str
 
+    # Porta utilizada pelo servidor MySQL.
     mysql_port: int = 3306
 
+    # Nome do banco de dados.
     mysql_database: str
 
+    # Usuário do banco.
     mysql_user: str
 
+    # Senha do banco.
     mysql_password: str
 
+    # Configuração do Pydantic.
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
