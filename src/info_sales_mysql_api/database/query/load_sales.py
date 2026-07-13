eb824de3754f8705eff695b8a0e8806639569b2a
jcarlossc <jcarlossc1977@gmail.com>
2026-07-13 15:27:22 -0300

    feat: Função load_sales adicionada

---
+++ src/info_sales_mysql_api/database/query/load_sales.py
@@ -0,0 +1,44 @@
+import logging
+import pandas as pd
+from sqlalchemy.engine import Engine
+# from sqlalchemy.exc import SQLAlchemyError
+
+
+def get_load_sales(engine: Engine) -> pd.DataFrame:
+    logger = logging.getLogger(__name__)
+
+    logger.info("Iniciando carregamento dos dados de vendas.")
+
+    query = """
+
+    SELECT
+    v.venda_id,
+    v.data_venda,
+    v.quantidade,
+    v.valor_compra,
+    v.valor_venda,
+    v.forma_pagamento,
+    v.desconto,
+    v.cidade,
+    v.estado,
+    v.status,
+
+    p.produto_id,
+    p.nome_produto,
+    p.categoria,
+
+    vd.vendedor_id,
+    vd.nome_vendedor
+
+    FROM vendas AS v
+
+    INNER JOIN produtos AS p
+        ON v.produto_id = p.produto_id
+
+    INNER JOIN vendedor AS vd
+        ON v.vendedor_id = vd.vendedor_id;
+    """
+
+    df = pd.read_sql(query, engine)
+
+    return df
